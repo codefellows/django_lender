93dc6043e430080bca8af79546d59685efd4287a
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-01-17 16:15:38 -0800

    travis attempt #2

--- lending_library/lending_library/settings.py
+++ lending_library/lending_library/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_lender',
-        'USER': 'Nick',
+        # 'USER': 'Nick',
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'TEST': {
